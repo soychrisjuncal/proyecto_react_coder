4062bcef7aa5f276a1ebe12f41460c4fa106c39d
soychrisjuncal <juncalcf@gmail.com>
2021-10-18 18:27:49 -0300

    personalize la creacion de la react app y cree una home en construccion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Tienda Online en Construcción.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/kiwi.boards/?hl=es-la"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visita Nuestras Redes
         </a>
       </header>
     </div>
