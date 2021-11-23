a598097f3877a9f538c9441edf9747f15767a64e
soychrisjuncal <juncalcf@gmail.com>
2021-11-23 01:25:50 -0300

    prbando context

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../css/Cart.css";
 import { CartContext } from "./CartContext";
 
 const Cart = ({ id, img, title, offerPrice, qty, children }) => {
   const test = useContext(CartContext);
+
   console.log("cart", test);
+  console.log(test.cartList);
+  console.log(test.cartList[0].imgItem);
 
   return (
     <>
       <h2>Carrito de Compras</h2>
       <div className="d-flex justify-content-center flex-column">
         <div className="d-flex justify-content-center">
-          <img src={test.img} className="error" alt="producto" />
+          <img
+            src={test.cartList[0].imgItem}
+            className="error"
+            alt="producto"
+          />
         </div>
 
         <h2 className="text-center">{children}</h2>
